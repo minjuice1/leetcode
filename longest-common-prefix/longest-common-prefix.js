46d43e9857e4c6b7d9222040c32ef2da1a0a4d14
ParkMinju <82799961+minjuice1@users.noreply.github.com>
2022-06-24 13:06:13 +0900

    Time: 87 ms, Memory: 42.3 MB - LeetHub

--- longest-common-prefix/longest-common-prefix.js
+++ longest-common-prefix/longest-common-prefix.js
@@ -1,15 +1,14 @@
-/**
- * @param {string[]} strs
- * @return {string}
- */
+
 var longestCommonPrefix = function(strs) {
-    let words = strs[0];
+    if(strs.length === 0) return "";
+    
+    let prefix = strs[0];
     
     for(let i = 1; i < strs.length; i++) {
-        while(strs[i].indexOf(words) != 0) {
-            words = words.substring(0, words.length - 1);
+        while(strs[i].indexOf(prefix) != 0) {
+            prefix = prefix.substring(0, prefix.length -1)
         }
     }
-    return words
+    return prefix
     
 };
