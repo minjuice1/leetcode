1c3ae93f264817ed6fb5fa4894a9eb4e54767d31
ParkMinju <82799961+minjuice1@users.noreply.github.com>
2022-06-26 14:26:33 +0900

    Time: 208 ms, Memory: 48.6 MB - LeetHub

--- array-partition-i/array-partition-i.js
+++ array-partition-i/array-partition-i.js
@@ -3,5 +3,6 @@
  * @return {number}
  */
 var arrayPairSum = function(nums) {
-    return nums.sort((a,b) => a-b).filter((a,i)=>!(i%2)).reduce((a,b)=>a+b,0)
+    // nums.sort((a,b) => a-b).filter((a,i)=>!(i%2)).reduce((a,b)=>a+b,0)
+    return nums.sort((a,b) => a-b).filter((a,i)=> !(i%2)).reduce((a,b)=>a+b)
 };
