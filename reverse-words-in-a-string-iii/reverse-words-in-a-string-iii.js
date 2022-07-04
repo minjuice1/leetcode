10bc0fa02f197731fc09a2440189952f29f561c1
ParkMinju <82799961+minjuice1@users.noreply.github.com>
2022-07-04 11:30:22 +0900

    Time: 77 ms, Memory: 48.5 MB - LeetHub

--- reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.js
+++ reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.js
@@ -3,16 +3,18 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    if(s === "") {
-        return -1;
-    }
+    if(s.length === 1) return s
     
-    let result;
-    result = s.split(" ")
-    for(let i = 0; i < result.length; i++) {
-        result[i] = result[i].split("").reverse().join("")
+    let splited = s.split(" ")
+    let result = [];
+    let store;
+    for(let i = 0; i < splited.length; i++) {
+        store = splited[i].split("")
+        store = store.reverse()
+        store = store.join("")
+        if(i !== splited.length-1)  store += " "
+        result += store
     }
-    
-    return result.join(" ")
+    return result
     
 };
